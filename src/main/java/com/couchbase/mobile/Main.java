5b66ab9e175ba34dc34de4d43f8e3dffe990a8e7
Hod Greeley <hod@greeley.org>
2017-03-01 11:02:23 -0800

    Added pom to build with maven.  Moved GUI fxml to resources subdir.  Changed load path to match.

--- src/main/java/com/couchbase/mobile/Main.java
+++ src/main/java/com/couchbase/mobile/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
     primaryStage.setTitle("Couchbase Mobile Changes Explorer");
     primaryStage.setScene(new Scene(root, 800, 300));
     primaryStage.show();
